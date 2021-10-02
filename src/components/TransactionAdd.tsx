622edab0bdbfca96dd955ff8546ae6a9cc098912
Brenna Switzer <brenna.switzer@gmail.com>
2021-10-02 12:50:56 -0700

    ts warnings

--- src/components/TransactionAdd.tsx
+++ src/components/TransactionAdd.tsx
@@ -1,14 +1,5 @@
 import React, { useState } from 'react';
-import {
-    Button,
-    TextField,
-    // FormControl,
-    // InputLabel,
-    Dialog,
-    DialogActions,
-    DialogContent,
-    DialogTitle,
-} from '@material-ui/core';
+import { Button, Dialog, DialogActions, DialogContent, DialogTitle, TextField } from '@material-ui/core';
 import { makeStyles } from '@material-ui/core/styles';
 import NumberFormat, { NumberFormatProps } from 'react-number-format';
 import { ToggleButton, ToggleButtonGroup } from '@material-ui/lab';
@@ -70,7 +61,7 @@ type Props = {
     account: string;
 };
 
-const TransactionAdd = ({ open, onClose, account }: Props) => {
+const TransactionAdd = ({ open, onClose, account }: Props): JSX.Element => {
     const classes = useStyles();
     const [description, setDescription] = useState('');
     const [type, setType] = useState('in');
