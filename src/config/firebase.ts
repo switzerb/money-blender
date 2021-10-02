34e0efa5bea36f960a6410750f034e2bd86e0793
Brenna Switzer <brenna.switzer@gmail.com>
2021-10-02 11:25:53 -0700

    Add react root

--- src/config/firebase.ts
+++ src/config/firebase.ts
@@ -2,13 +2,13 @@ import firebase from 'firebase/app';
 import 'firebase/firestore';
 
 const firebaseConfig = {
-    apiKey: "AIzaSyCUSsqIBq-yP72a3mYyCAhkFbMoKBlTQbM",
-    authDomain: "money-blender-bf6f8.firebaseapp.com",
+    apiKey: 'AIzaSyCUSsqIBq-yP72a3mYyCAhkFbMoKBlTQbM',
+    authDomain: 'money-blender-bf6f8.firebaseapp.com',
     databaseURL: 'https://money-blender-bf6f8.firebaseio.com',
-    projectId: "money-blender-bf6f8",
-    storageBucket: "money-blender-bf6f8.appspot.com",
-    messagingSenderId: "372394001654",
-    appId: "1:372394001654:web:519360d605aeb968e8d8d7"
+    projectId: 'money-blender-bf6f8',
+    storageBucket: 'money-blender-bf6f8.appspot.com',
+    messagingSenderId: '372394001654',
+    appId: '1:372394001654:web:519360d605aeb968e8d8d7',
 };
 
 if (!firebase.apps.length) {
